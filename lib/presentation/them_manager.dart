a82e4cf37522e619e9bd5f72b7600641c6c6df0f
mahmoudvolt <mahmodhoda328@gmail.com>
2022-08-31 15:40:45 +0200

    text form field

--- lib/presentation/them_manager.dart
+++ lib/presentation/them_manager.dart
@@ -75,5 +75,69 @@ ThemeData getAppThem() {
         color: ColorManager.grey,
       ),
     ),
+
+    // input decoration them (text form field)
+
+    inputDecorationTheme: InputDecorationTheme(
+      // content padding
+      contentPadding: const EdgeInsets.all(
+        AppPadding.p8,
+      ),
+      hintStyle: getRegularStyle(
+        color: ColorManager.grey,
+        fontSize: FontSize.s14,
+      ),
+      labelStyle: getMediumStyle(
+        color: ColorManager.grey,
+        fontSize: FontSize.s14,
+      ),
+      errorStyle: getRegularStyle(
+        color: ColorManager.error,
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: ColorManager.primary,
+          width: AppSize.s1_5,
+        ),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(
+            AppSize.s8,
+          ),
+        ),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: ColorManager.grey,
+          width: AppSize.s1_5,
+        ),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(
+            AppSize.s8,
+          ),
+        ),
+      ),
+      errorBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: ColorManager.error,
+          width: AppSize.s1_5,
+        ),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(
+            AppSize.s8,
+          ),
+        ),
+      ),
+      focusedErrorBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: ColorManager.primary,
+          width: AppSize.s1_5,
+        ),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(
+            AppSize.s8,
+          ),
+        ),
+      ),
+    ),
   );
 }
