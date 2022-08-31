7a0db117d2f09ad8c1dbb23508a66d0e86fd0b5f
mahmoudvolt <mahmodhoda328@gmail.com>
2022-08-31 15:10:49 +0200

    app bar them

--- lib/presentation/them_manager.dart
+++ lib/presentation/them_manager.dart
@@ -1,21 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_advanced/presentation/color_manager.dart';
+import 'package:flutter_advanced/presentation/font_manager.dart';
+import 'package:flutter_advanced/presentation/styles_manager.dart';
 import 'package:flutter_advanced/presentation/values_manager.dart';
 
 ThemeData getAppThem() {
   return ThemeData(
-      // main colors
+    // main colors
 
-      primaryColor: ColorManager.primary,
-      primaryColorLight: ColorManager.lightPrimary,
-      primaryColorDark: ColorManager.darkPrimary,
-      disabledColor: ColorManager.grey1,
+    primaryColor: ColorManager.primary,
+    primaryColorLight: ColorManager.lightPrimary,
+    primaryColorDark: ColorManager.darkPrimary,
+    disabledColor: ColorManager.grey1,
+    splashColor: ColorManager.lightPrimary,
 
-      // card view
+    // card view
 
-      cardTheme: CardTheme(
+    cardTheme: CardTheme(
+      color: ColorManager.white,
+      shadowColor: ColorManager.grey,
+      elevation: AppSize.s4,
+    ),
+
+    // app bar them
+    appBarTheme: AppBarTheme(
+      centerTitle: true,
+      color: ColorManager.primary,
+      elevation: AppSize.s4,
+      shadowColor: ColorManager.lightPrimary,
+      titleTextStyle: getRegularStyle(
         color: ColorManager.white,
-        shadowColor: ColorManager.grey,
-        elevation: AppSize.s4,
-      ));
+        fontSize: FontSize.s16,
+      ),
+    ),
+  );
 }
