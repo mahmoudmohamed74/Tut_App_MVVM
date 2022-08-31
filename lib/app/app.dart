34963d685a095a05f287c29116f238f0edceb103
mahmoudvolt <mahmodhoda328@gmail.com>
2022-08-31 08:24:23 +0200

    first commit

---
+++ lib/app/app.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+  MyApp._internal();
+  int appState = 0;
+  static final MyApp _instance = MyApp._internal();
+
+  factory MyApp() => _instance;
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class Test extends StatelessWidget {
+  const Test({Key? key}) : super(key: key);
+
+  void updateAppState() {
+    MyApp().appState = 11;
+  }
+
+  void printAppState() {
+    print(MyApp().appState);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class Test2 extends StatelessWidget {
+  const Test2({Key? key}) : super(key: key);
+  void printAppState() {
+    print(MyApp().appState);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
