e6159798e5382072a8907335eb32b4bbcdf640ed
mahmoudvolt <mahmodhoda328@gmail.com>
2022-08-31 15:05:08 +0200

    card them

--- lib/presentation/them_manager.dart
+++ lib/presentation/them_manager.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_advanced/presentation/color_manager.dart';
+import 'package:flutter_advanced/presentation/values_manager.dart';
 
 ThemeData getAppThem() {
   return ThemeData(
-    // main colors
+      // main colors
 
-    primaryColor: ColorManager.primary,
-    primaryColorLight: ColorManager.lightPrimary,
-    primaryColorDark: ColorManager.darkPrimary,
-    disabledColor: ColorManager.grey1,
-  );
+      primaryColor: ColorManager.primary,
+      primaryColorLight: ColorManager.lightPrimary,
+      primaryColorDark: ColorManager.darkPrimary,
+      disabledColor: ColorManager.grey1,
+
+      // card view
+
+      cardTheme: CardTheme(
+        color: ColorManager.white,
+        shadowColor: ColorManager.grey,
+        elevation: AppSize.s4,
+      ));
 }
