6212d9b429b6b04f58db8a09893913d46ffc7b9e
mahmoudvolt <mahmodhoda328@gmail.com>
2022-09-01 16:37:34 +0200

    create base model

---
+++ lib/presentation/base/base_view_model.dart
@@ -0,0 +1,11 @@
+abstract class BaseViewModel {
+  // shared variables and func at any view model
+}
+
+abstract class BaseViewModelInputs {
+  void start();
+
+  void dispose();
+}
+
+abstract class BaseViewModelOutputs {}
