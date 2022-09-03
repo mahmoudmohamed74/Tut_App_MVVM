d26011133d44ac3a1866fb5bbeac6b4b9b1e106c
mahmoudvolt <mahmodhoda328@gmail.com>
2022-09-03 12:02:37 +0200

    model response & extensions

--- lib/domain/model.dart
+++ lib/domain/model.dart
@@ -14,3 +14,23 @@ class SliderViewObject {
 
   SliderViewObject(this.sliderObject, this.numOfSlides, this.currentIndex);
 }
+
+class Customer {
+  String id;
+  String name;
+  int numOfNotifications;
+  Customer(this.id, this.name, this.numOfNotifications);
+}
+
+class Contacts {
+  String phone;
+  String email;
+  String link;
+  Contacts(this.phone, this.email, this.link);
+}
+
+class Authentication {
+  Customer? customer;
+  Contacts? contacts;
+  Authentication(this.contacts, this.customer);
+}
