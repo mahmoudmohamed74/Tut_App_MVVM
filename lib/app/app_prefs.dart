de9bc826586da692cec1a348071809cfde32ac0c
mahmoudvolt <mahmodhoda328@gmail.com>
2022-09-05 17:44:19 +0200

    dio factory

--- lib/app/app_prefs.dart
+++ lib/app/app_prefs.dart
@@ -7,13 +7,13 @@ class AppPreferences {
   final SharedPreferences _sharedPreferences;
 
   AppPreferences(this._sharedPreferences);
-  Future<String?> getAppLanguage() async {
+  Future<String> getAppLanguage() async {
     String? language = _sharedPreferences.getString(prefsAppKey);
     if (language != null && language.isNotEmpty) {
       return language;
     } else {
       // return default language
-      LanguageType.ENGLISH.getValue();
+      return LanguageType.ENGLISH.getValue();
     }
   }
 }
--- lib/data/network/dio_factory.dart
+++ lib/data/network/dio_factory.dart
@@ -2,6 +2,7 @@
 
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter_advanced/app/app_prefs.dart';
 import 'package:flutter_advanced/app/constants.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
@@ -12,14 +13,18 @@ const String AUTHORIZATION = "authorization";
 const String DEFAULT_LANG = "language";
 
 class DioFactory {
+  final AppPreferences _appPreferences;
+  DioFactory(this._appPreferences);
   Future<Dio> getDio() async {
     Dio dio = Dio();
+    String language = await _appPreferences
+        .getAppLanguage(); // get the saved lang that user saved in sheared pref
 
     Map<String, String> headers = {
       CONTENT_TYPE: APPLICATION_JSON,
       ACCEPT: APPLICATION_JSON,
       AUTHORIZATION: Constants.token,
-      DEFAULT_LANG: "en",
+      DEFAULT_LANG: language,
     };
 
     dio.options = BaseOptions(
