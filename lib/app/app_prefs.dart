ba99a96e1bc238e7a6fbf5681b5040db2b0f1fb6
mahmoudvolt <mahmodhoda328@gmail.com>
2022-09-05 16:44:34 +0200

    shared prefs

---
+++ lib/app/app_prefs.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_advanced/presentation/resources/language_manager.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+const String prefsAppKey = "";
+
+class AppPreferences {
+  final SharedPreferences _sharedPreferences;
+
+  AppPreferences(this._sharedPreferences);
+  Future<String?> getAppLanguage() async {
+    String? language = _sharedPreferences.getString(prefsAppKey);
+    if (language != null && language.isNotEmpty) {
+      return language;
+    } else {
+      // return default language
+      LanguageType.ENGLISH.getValue();
+    }
+  }
+}
