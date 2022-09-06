3c19dcf095d1fe12ca78e6f099c0f75f98975e2e
mahmoudvolt <mahmodhoda328@gmail.com>
2022-09-06 18:50:09 +0200

    freezed data

--- lib/presentation/login/viewmodel/login_viewmodel.dart
+++ lib/presentation/login/viewmodel/login_viewmodel.dart
@@ -31,14 +31,12 @@ class LoginViewModel extends BaseViewModel
 
   @override
   setPassword(String password) {
-    // TODO: implement setPassword
-    throw UnimplementedError();
+    inputPassword.add(password);
   }
 
   @override
   setUserName(String userName) {
-    // TODO: implement setUserName
-    throw UnimplementedError();
+    inputUserName.add(userName); // ناخده من اليوزر
   }
 
   @override
