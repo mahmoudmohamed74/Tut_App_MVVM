e91dc9437851502a36121295da557972b0f17d70
mahmoudvolt <mahmodhoda328@gmail.com>
2022-09-08 22:33:18 +0200

    complete di

--- lib/app/di.dart
+++ lib/app/di.dart
@@ -1,22 +1,53 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_advanced/app/app_prefs.dart';
+import 'package:flutter_advanced/data/data_source/remote_data_source.dart';
+import 'package:flutter_advanced/data/network/app_api.dart';
+import 'package:flutter_advanced/data/network/dio_factory.dart';
+import 'package:flutter_advanced/data/network/network_info.dart';
+import 'package:flutter_advanced/data/repository/repository_impl.dart';
+import 'package:flutter_advanced/domain/repository/repository.dart';
 import 'package:get_it/get_it.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 final instance = GetIt.instance;
 
 Future<void> initAppModule() async
-//save all generic dependencies will be used in all app
+// save all generic dependencies will be used in all app
 
 {
-  //shared prefs instance
+  // shared prefs instance
 
   final sherdPrefs = await SharedPreferences.getInstance();
   instance.registerLazySingleton<SharedPreferences>(() => sherdPrefs);
 
-  //app prefs instance
+  // app prefs instance
 
   instance.registerLazySingleton<AppPreferences>(
     () => AppPreferences(instance()),
+  );
+
+  // network info
+  instance.registerLazySingleton<NetworkInfo>(
+    () => NetworkInfoImpl(InternetConnectionChecker()),
+  );
+  // dio factory
+  instance.registerLazySingleton<DioFactory>(
+    () => DioFactory(instance()),
+  );
+  Dio dio = await instance<DioFactory>().getDio();
+  // app service client
+  instance.registerLazySingleton<AppServiceClient>(
+    () => AppServiceClient(dio),
+  );
+// remote data source
+  instance.registerLazySingleton<RemoteDataSource>(
+    () => RemoteDataSourceImpl(instance()),
+  );
+
+  // repository
+  instance.registerLazySingleton<Repository>(
+    () => RepositoryImpl(instance(), instance()),
   );
 }
 
