6f3274a1e4a7ce8321a75a861af094ea2bc5035a
mahmoudvolt <mahmodhoda328@gmail.com>
2022-09-17 00:12:36 +0200

    error / content / empty state renderer

--- lib/presentation/common/state_renderer/state_randerer_impl.dart
+++ lib/presentation/common/state_renderer/state_randerer_impl.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_advanced/app/constants.dart';
 import 'package:flutter_advanced/presentation/common/state_renderer/state_renderer.dart';
 import 'package:flutter_advanced/presentation/resources/strings_manager.dart';
 
@@ -20,4 +21,40 @@ class LoadingState extends FlowState {
 
   @override
   StateRendererType getStateRendererType() => stateRendererType;
+}
+
+// error (popup , fullState)
+
+class ErrorState extends FlowState {
+  StateRendererType stateRendererType;
+  String message;
+  ErrorState(this.stateRendererType, this.message);
+  @override
+  String getMessage() => message;
+
+  @override
+  StateRendererType getStateRendererType() => stateRendererType;
+}
+
+// Content (popup , fullState)
+
+class ContentState extends FlowState {
+  ContentState();
+  @override
+  String getMessage() => Constants.empty;
+
+  @override
+  StateRendererType getStateRendererType() => StateRendererType.contentState;
+}
+
+// Empty (popup , fullState)
+
+class EmptyState extends FlowState {
+  String message;
+  EmptyState(this.message);
+  @override
+  String getMessage() => message;
+
+  @override
+  StateRendererType getStateRendererType() => StateRendererType.contentState;
 }
