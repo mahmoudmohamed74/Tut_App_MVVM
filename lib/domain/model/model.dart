09afdfd97ff1da15c0b6a415ed9b2f4320c22d93
mahmoudvolt <mahmodhoda328@gmail.com>
2022-09-28 02:28:25 +0200

    home models

--- lib/domain/model/model.dart
+++ lib/domain/model/model.dart
@@ -35,3 +35,38 @@ class Authentication {
   Contacts? contacts;
   Authentication(this.contacts, this.customer);
 }
+
+class Service {
+  String id;
+  String title;
+  String image;
+  Service(this.id, this.title, this.image);
+}
+
+class BannerAd {
+  String id;
+  String link;
+  String title;
+  String image;
+
+  BannerAd(this.id, this.link, this.title, this.image);
+}
+
+class Store {
+  String id;
+  String title;
+  String image;
+  Store(this.id, this.title, this.image);
+}
+
+class HomeData {
+  List<Service> services;
+  List<BannerAd> banners;
+  List<Store> stores;
+  HomeData(this.services, this.banners, this.stores);
+}
+
+class HomeObject {
+  HomeData? data;
+  HomeObject(this.data);
+}
