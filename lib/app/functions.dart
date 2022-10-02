e34a5886a8cd71ebf8f07bacce7a755b31fc2c4e
mahmoudvolt <mahmodhoda328@gmail.com>
2022-10-02 18:50:03 +0200

    test

--- lib/app/functions.dart
+++ lib/app/functions.dart
@@ -1,5 +1,5 @@
 bool isEmailValid(String email) {
   return RegExp(
-          r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-      .hasMatch(email);
+    r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
+  ).hasMatch(email);
 }
